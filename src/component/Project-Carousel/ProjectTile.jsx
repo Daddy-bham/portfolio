5501468a630f98bb2d300689e909bdbeffe9e0e9
karuneshT <69667224+karuneshT@users.noreply.github.com>
2021-09-03 14:15:58 +0530

    Update ProjectTile.jsx
    edited changes regarding project image

--- src/component/Project-Carousel/ProjectTile.jsx
+++ src/component/Project-Carousel/ProjectTile.jsx
@@ -4,7 +4,6 @@ function ProjectTile(props){
     return(
         <div className="d-flex py-2 col-sm-12 justify-content-center col-md-4 col-lg-3">
             <div className="project-tile">
-                <img className="project-pic mx-1 " src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752" alt="test" />
                 <div className="text-center bg-warning text-white">
                     <h2>{props.title}</h2>
                 </div>
